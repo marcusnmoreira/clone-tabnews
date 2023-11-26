53e5f4e1ab003fc7433daa16f4b51758bee3c6e1
Marcus <marcusvnm.moreira@gmail.com>
2023-11-26 22:47:38 +0000

    Modifica h1 da página principal

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home () {
-    return <h1 style={{color: 'blue'}}>Agora vai!</h1>
+    return (
+        <>
+            <h1>Agora vai!</h1>
+            <p>Mais um deploy com sucesso</p>
+        </>
+    )
+
 }
 
 export default Home;
