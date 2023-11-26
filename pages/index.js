5f28ca95f837623eddb12fe7324c653b4509aa69
Marcus <marcusvnm.moreira@gmail.com>
2023-11-26 22:59:41 +0000

    modifica estilização pages

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 function Home () {
+    const containerStyle = {
+        display: 'flex',
+        alignItems: 'center',
+    };
+
     return (
-        <>
+        <div style={containerStyle}>
             <h1>Agora vai!</h1>
             <p>Mais um deploy com sucesso</p>
-        </>
+        </div>
     )
 
 }
