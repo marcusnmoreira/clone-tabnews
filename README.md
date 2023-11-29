3f2e750e93bdaad48b37c7dfc35e71bc537eb718
Marcus <marcusvnm.moreira@gmail.com>
2023-11-29 12:57:26 +0000

    Adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto para teste e aprendizagem
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,18 @@
-function Home () {
-    const containerStyle = {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        textAlign: 'center',
-    };
+function Home() {
+  const containerStyle = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+  };
 
-    return (
-        <div style={containerStyle}>
-            <h1>Agora vai!</h1>
-            <p style={{marginTop: 0}}>Mais um deploy com sucesso</p>
-        </div>
-    )
-
+  return (
+    <div style={containerStyle}>
+      <h1>Agora vai!</h1>
+      <p style={{ marginTop: 0 }}>Mais um deploy com sucesso</p>
+    </div>
+  );
 }
 
 export default Home;
